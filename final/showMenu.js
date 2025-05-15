b61a5872eaebe376a03f764aa703887d2b660100
giarnella <giarnella.talia@gmail.com>
2025-05-15 13:28:14 -0400

    remove-statenisland

--- final/showMenu.js
+++ final/showMenu.js
@@ -1,8 +1,8 @@
 function showMenu(){
     var  menu = document.getElementById("top-nav");
-    if  (menu.style.display==="none" || menu.style.display === ''){
-        menu.style.display="block";
+    if  (menu.style.top==="-200px" || menu.style.top === ''){
+        menu.style.top="30px";
     } else {
-    menu.style.display="none";
+    menu.style.top="-200px";
         }
     }
